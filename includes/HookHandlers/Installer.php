c3936702fc86cdb23f40c3f6e4cda7271e649cc4
TehKittyCat <tehkittycat@weirdgloop.org>
2026-01-23 01:39:18 -0500

    WGL only - Avoid creating a bunch of jobs while trying to upgrade mediawiki. We can run this script later to avoid hammering DB.

--- includes/HookHandlers/Installer.php
+++ includes/HookHandlers/Installer.php
@@ -15,7 +15,8 @@ class Installer implements LoadExtensionSchemaUpdatesHook {
 	 * @codeCoverageIgnore Tested by updating or installing MediaWiki.
 	 */
 	public function onLoadExtensionSchemaUpdates( $updater ) {
-		$updater->addPostDatabaseUpdateMaintenance( DeleteTemplate::class );
+		// WGL - Avoid creating a bunch of jobs while trying to upgrade mediawiki. We can run this script later to avoid hammering DB.
+		//$updater->addPostDatabaseUpdateMaintenance( DeleteTemplate::class );
 		$updater->addPostDatabaseUpdateMaintenance( DropView::class );
 	}
 }
